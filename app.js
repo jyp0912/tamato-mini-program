97a0a708f7396c48418d23e7349972b16849e5b6
jyp0912 <50137176+jyp0912@users.noreply.github.com>
2020-03-30 18:58:07 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-      host: 'https://gp-server.hunger-valley.com',  //照抄
-      app_id: "wx51eefffc9053958c",
-      app_secret: "4ea3d7248f19bf8a6dba4fb7cfdfe61e",
-      t_app_id: "UtmjM6HmckiMoCvbu2rqAdCC",
-      t_app_secret: "sp6RyvNjeFwvVvRF5Zzhumj9"
-  }
- })
