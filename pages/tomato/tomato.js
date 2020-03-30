d6d986a11b7b7870123007710133b5505727e36d
oliver <896986794@qq.com>
2020-03-30 17:20:26 +0800

    完成静态页面

--- pages/tomato/tomato.js
+++ pages/tomato/tomato.js
@@ -1,10 +1,12 @@
 Page({
   timer:null,
   data: {
-    defaultSec:1500,
+    defaultSec:3,
     time:"",
     timeStatus:'start',
-    confirmVisible:false
+    confirmVisible:false,
+    againVisible:false,
+    finishConfirmVisible:false
   },
   onShow:function(){
     this.countDown()
@@ -13,17 +15,24 @@ Page({
     this.setData({timeStatus:'stop'})
     this.changeTime()
     this.timer =setInterval(() => {
-      if (this.data.defaultSec === 0){
-        return this.clearTimer()
-      }
       this.data.defaultSec -- 
       this.changeTime()
+      if (this.data.defaultSec <= 0){
+        this.setData({againVisible:true})
+        this.setData({finishConfirmVisible:true})
+        return this.clearTimer()
+      }
     }, 1000);
   },
   clearTimer(){
     clearInterval(this.timer)
     this.timer=null,
     this.setData({timeStatus:'start'})
+  },
+  againTimer(){
+    this.data.defaultSec=10
+    this.countDown()
+    this.setData({againVisible:false})
   },
   changeTime(){
     let min = Math.floor(this.data.defaultSec/60)
@@ -38,11 +47,25 @@ Page({
     }
     this.setData({time:`${min}:${sec}`})
     },
+    confirmFinish(event){
+      let content = event.detail
+    },
+    confirmCancel(){
+      this.setData({ finishConfirmVisible: false })
+    },
+    confirmAbandon(event){
+      let content = event.detail
+      wx.navigateBack({
+        to:-1
+      })
+    },
     showConfirm(){
       this.setData({confirmVisible:true})
+      this.clearTimer()
     },
     hideConfirm(){
       this.setData({confirmVisible:false})     
+      this.countDown()
     }
   ,
 
